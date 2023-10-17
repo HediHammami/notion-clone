dd56734c246b5f2f8d964bcf52c31be7a9618a33
HediHammami <hedihammami95@gmail.com>
2023-10-17 14:56:56 +0100

    my first commit

---
+++ src/components/CreateNoteDialog.tsx
@@ -0,0 +1,106 @@
+"use client";
+import React, { useState } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
+import { Loader2, Plus } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+
+type Props = {};
+
+const CreateNoteDialog = (props: Props) => {
+  const router = useRouter();
+  const [input, setInput] = useState(""); 
+   
+  const uploadToFirebase = useMutation({
+    mutationFn: async (noteId: string) => {
+      const response = await axios.post("/api/uploadToFirebase", {
+        noteId,
+      });
+      return response.data;
+    },
+  });
+
+  const createNoteBook = useMutation({
+    mutationFn: async () => {
+      const response = await axios.post("/api/createNoteBook", {
+        name: input,
+      });
+      return response.data;
+    },
+  });
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (input === "") {
+      return alert("Please enter a name for your note book.");
+    }
+
+    createNoteBook.mutate(undefined, {
+      onSuccess: (note_id) => {
+        console.log("Created new note:", { note_id });
+        uploadToFirebase.mutate(note_id);
+        router.push(`/notebook/${note_id}`);
+      },
+      onError: (error) => {
+        console.log(error);
+        window.alert("Failed to create a new note book");
+      },
+    });
+  };
+  return (
+    <Dialog>
+      <DialogTrigger>
+        <div className="border-dashed border-2 flex border-green-600 h-full rounded-lg items-center justify-center sm:flex-col hover:shadow-xl transition hover:-translate-y-1 flex-row p-4">
+          <Plus className="w-6 h-6 text-green-600" strokeWidth={3} />
+          <h2 className="font-semibold text-green-600 sm:mt-2">
+            New Note Book
+          </h2>
+        </div>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>New Note Book</DialogTitle>
+          <DialogDescription>
+            You can create a new note by clicking the button below.
+          </DialogDescription>
+        </DialogHeader>
+
+        <form onSubmit={handleSubmit}>
+          <Input
+            placeholder="Name..."
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <div className="h-4"></div>
+          <div className="flex items-center gap-2">
+            <Button type="reset" variant={"secondary"}>
+              Cancel
+            </Button>
+            <Button
+              type="submit"
+              className="bg-green-600"
+              disabled={createNoteBook.isLoading}
+            >
+              {createNoteBook.isLoading && (
+                <Loader2 className="w-4 h-4 mr-2 animate-spin" />
+              )}
+              Create
+            </Button>
+          </div>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default CreateNoteDialog;
---
+++ src/lib/firebase.ts
@@ -0,0 +1,30 @@
+import { initializeApp } from "firebase/app";
+import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA01ixvGJi7rho9CbaITRds_oZ_Kesm_bo",
+  authDomain: "notion-blog-2a4c5.firebaseapp.com",
+  projectId: "notion-blog-2a4c5",
+  storageBucket: "notion-blog-2a4c5.appspot.com",
+  messagingSenderId: "1092716886397",
+  appId: "1:1092716886397:web:b04c980a50cc3bd1dcb414",
+};
+
+const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app);
+
+export async function uploadFileToFirebase(image_url: string, name: string) {
+  try {
+    const response = await fetch(image_url);
+    const buffer = await response.arrayBuffer();
+    const file_name = name.replace(" ", "") + Date.now + ".jpeg";
+    const storageRef = ref(storage, file_name);
+    await uploadBytes(storageRef, buffer, {
+      contentType: "image/jpeg",
+    });
+    const firebase_url = await getDownloadURL(storageRef);
+    return firebase_url;
+  } catch (error) {
+    console.error(error);
+  }
+}
